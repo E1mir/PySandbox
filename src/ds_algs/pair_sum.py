3d74c6cb48f4f795e8dc6ed7cf7f1186d4f54a43
Elmir I-de <kryternext@gmail.com>
2018-08-17 22:53:18 +0400

    Pair sum problem

---
+++ src/ds_algs/pair_sum.py
@@ -0,0 +1,50 @@
+"""
+Problem
+Given an integer array, output all the * *unique ** pairs that sum up to a specific value k.
+
+So the input:
+
+pair_sum([1,3,2,2],4)
+would return 2 pairs:
+
+ (1,3)
+ (2,2)
+NOTE: FOR TESTING PURPOSES< CHANGE YOUR FUNCTION SO IT OUTPUTS THE NUMBER OF PAIRS
+"""
+
+from nose.tools import assert_equal
+
+
+class TestPair(object):
+
+    def test(self, sol):
+        assert_equal(sol([1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1], 10), 6)
+        assert_equal(sol([1, 2, 3, 1], 3), 1)
+        assert_equal(sol([1, 3, 2, 2], 4), 2)
+        print('ALL TEST CASES PASSED')
+
+
+def pair_sum(arr, s):
+    if len(arr) < 2:
+        return
+
+    seen = set()
+    output = set()
+
+    for num in arr:
+        target = s - num
+
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add(
+                (min(target, num), max(target, num))
+            )
+
+    return len(output)
+
+
+if __name__ == '__main__':
+    t = TestPair()
+
+    t.test(pair_sum)
