3f209b114873c400325a5c4aa5d04d2cdaf98b3b
Elmir I-de <kryternext@gmail.com>
2018-09-02 01:47:34 +0400

    Tree data structure with some example tree for traversal testing

---
+++ src/problems/binary_trees/tree_traversals.py
@@ -0,0 +1,60 @@
+from problems.binary_trees.tree import BinaryTree
+
+if __name__ == '__main__':
+    r = BinaryTree("Main root")
+    r.insert_left(1)
+    r.insert_right(2)
+
+    # Left child of the main root
+    b_1 = r.get_left_child()
+    # Right child of the main root
+    b_2 = r.get_right_child()
+
+    b_1.insert_left(11)
+    b_1.insert_right(12)
+    b_2.insert_left(21)
+    b_2.insert_right(22)
+
+    # Left child of b_1
+    b_1_1 = b_1.get_left_child()
+    # Right child of b_1
+    b_1_2 = b_1.get_left_child()
+    # Left child of b_2
+    b_2_1 = b_2.get_left_child()
+    # Right child of b_2
+    b_2_2 = b_2.get_right_child()
+
+    b_1_1.insert_left(111)
+    b_1_1.insert_right(112)
+    b_1_2.insert_left(121)
+    b_1_2.insert_right(122)
+    b_2_1.insert_left(211)
+    # b_2_1 has not a right child
+    b_2_2.insert_left(221)
+    b_2_2.insert_right(222)
+
+    # Left child of b_1_1
+    b_1_1_1 = b_1_1.get_left_child()
+    # Right child of b_1_1
+    b_1_1_2 = b_1_1.get_right_child()
+    # Left child of b_1_2
+    b_1_2_1 = b_1_2.get_left_child()
+    # Right child of b_1_2
+    b_1_2_2 = b_1_2.get_right_child()
+    # Left child of b_2_1
+    b_2_1_1 = b_2_1.get_left_child()
+    # b_2_1 has not right child
+    # Left child of b_2_2
+    b_2_2_1 = b_2_2.get_left_child()
+    # Right child of b_2_2
+    b_2_2_2 = b_2_2.get_right_child()
+
+    b_1_1_1.insert_left(1111)
+    # b_1_1_1 has not right child
+    b_1_1_2.insert_left(1121)
+    b_1_1_2.insert_right(1122)
+    # b_2_1_1 has not child
+    b_2_2_1.insert_left(2211)
+    b_2_2_1.insert_right(2212)
+    # b_2_2_2 has not left child
+    b_2_2_2.insert_right(2222)
