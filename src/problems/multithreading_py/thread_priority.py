580851aae5c647e917f39e8962570bde4b863060
Elmir I-de <kryternext@gmail.com>
2018-09-03 17:52:41 +0400

    Update thread priority

--- src/problems/multithreading_py/thread_priority.py
+++ src/problems/multithreading_py/thread_priority.py
@@ -1,56 +1,33 @@
-import queue
-import threading
-
-exit_flag = False
+import queue, threading, time
 
 
 class MyThread(threading.Thread):
-
-    def __init__(self, thread_id, name, q):
-        threading.Thread.__init__(self)
-        self.thread_id = thread_id
-        self.name = name
+    def __init__(self, q, name):
+        threading.Thread.__init__(self, name=name, daemon=True)
         self.q = q
 
     def run(self):
         print("Starting {}".format(self.name))
-        process_data(self.name, self.q)
+        while True:
+            data = self.q.get()
+            if data is None:
+                break
+            print("{} processing {}".format(self.name, data))
+            time.sleep(2)
+            self.q.task_done()
         print("Exiting {}".format(self.name))
-
-
-def process_data(working_thread, q):
-    while not exit_flag:
-        queue_lock.acquire()
-        if not work_queue.empty():
-            data = q.get()
-            queue_lock.release()
-            print("{} processing {}".format(working_thread, data))
 
 
 thread_list = ["Thread-1", "Thread-2", "Thread-3"]
 name_list = ["One", "Two", "Three", "Four", "Five"]
 
-queue_lock = threading.Lock()
-work_queue = queue.Queue(10)
-threads = []
-t_id = 1
-
-for t_name in thread_list:
-    thread = MyThread(t_id, t_name, work_queue)
+work_queue = queue.Queue()
+threads = [MyThread(work_queue, t_name) for t_name in thread_list]
+for thread in threads:
     thread.start()
-    threads.append(thread)
-    t_id += 1
 
-queue_lock.acquire()
 for word in name_list:
     work_queue.put(word)
-queue_lock.release()
+work_queue.join()
 
-while not work_queue.empty():
-    pass
-
-exit_flag = True
-
-for t in threads:
-    t.join()
-print("Exit main thread")
+print("Main thread end!")
