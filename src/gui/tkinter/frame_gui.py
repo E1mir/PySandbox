d3258a06f00ecb11e396270c47b4d9d991d69e93
Elmir I-de <kryternext@gmail.com>
2018-09-14 17:51:24 +0400

    Tkinter frame

---
+++ src/gui/tkinter/frame_gui.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+window = Tk()
+
+frame = Frame(window)
+frame.pack()
+
+btn = Button(frame, text="Hello")
+btn.pack(side=LEFT)
+
+cv = IntVar()
+cb = Checkbutton(
+    frame, text="Text",
+    variable=cv,
+    onvalue=1,
+    offvalue=0,
+    height=6,
+    width=18
+)
+cb.pack()
+
+window.mainloop()
