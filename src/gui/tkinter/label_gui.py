244527e8d991a65fd0cb08a687654546d0b8d87b
Elmir I-de <kryternext@gmail.com>
2018-09-14 17:54:24 +0400

    Tkinter simple label

---
+++ src/gui/tkinter/label_gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+
+sv = StringVar()
+lbl = Label(window, textvariable=sv)
+sv.set('Awesome label text')
+print(sv.get())
+lbl.pack(anchor=CENTER)
+window.mainloop()
